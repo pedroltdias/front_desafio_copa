04e23290c82bb71ce99cf79d93f1280f44612da8
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-12-20 15:39:11 -0300

    construcao da navbar e exibicao de equipes

---
+++ src/components/AppNav.vue
@@ -0,0 +1,49 @@
+<template>
+	<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="https://bulma.io">
+				<img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+			</a>
+
+			<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+				data-target="navbarBasicExample">
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+			</a>
+		</div>
+
+		<div class="navbar-menu">
+			<div class="navbar-start">
+				<a class="navbar-item">
+					Partidas
+				</a>
+				<a class="navbar-item">
+					Times
+				</a>
+				<a class="navbar-item">
+					Jogadores
+				</a>
+			</div>
+
+			<div class="navbar-end">
+				<div class="navbar-item">
+					<a class="navbar-item">
+						Usuário
+					</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: "AppNav"
+})
+
+
+</script>
---
+++ src/components/Teams.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="container">
+		<div class="is-flex is-justify-content-space-between is-align-items-center">
+			<h2 class="is-size-2">Times</h2>
+			<button class="button" @click="toggleModal">Adicionar</button>
+		</div>
+		<!-- Extrair esse codigo para o componente AppModal -->
+		<div class="modal" :class="{ 'is-active': showModal }">
+			<div class="modal-background"></div>
+			<div class="modal-content">
+				<form action="" class="box">
+					<h3 class="is-size-3">Adicionar Novo Time</h3>
+					<div class="field">
+						<label class="label">Nome do Time</label>
+						<div class="control">
+							<input type="text" class="input" placeholder="Nome do time...">
+						</div>
+					</div>
+					<button class="button is-primary">Salvar</button>
+				</form>
+			</div>
+			<button class="modal-close is-large" @click="toggleModal" aria-label="close"></button>
+		</div>
+		<!-- Até aqui -->
+		<TeamBox />
+		<TeamBox />
+		<TeamBox />
+	</div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import TeamBox from './TeamBox.vue';
+// import AppModal from './AppModal.vue';
+
+export default defineComponent({
+	name: "MeusTimes",
+	components: {
+		TeamBox,
+		// AppModal
+	},
+	data() {
+		return {
+			showModal: false
+		}
+	},
+	methods: {
+		toggleModal() {
+			this.showModal = !this.showModal
+		}
+	}
+})
+
+
+</script>
